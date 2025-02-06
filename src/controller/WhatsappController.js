9666a4656b8a364723b4defbdd14086ac19007de
jaumrosa <joao.rosa5@outlook.com>
2025-02-06 18:50:20 -0300

    Criando eventos para abertura e fechamento dos painéis 'Editar perfil' e 'Adicionar Contato'

--- src/controller/WhatsappController.js
+++ src/controller/WhatsappController.js
@@ -6,6 +6,7 @@ class WhatsappController {
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
     }
 
     loadElements() {
@@ -74,6 +75,49 @@ class WhatsappController {
             return this.classList.contains(name);
         }
 
+    }
+
+    initEvents() {
+
+        this.el.myPhoto.on('click', e => {
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+                
+            }, 300);
+            
+
+        });
+
+        this.el.btnNewContact.on('click', e => {
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+
+            }, 300);
+            
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+
+            this.el.panelEditProfile.removeClass('open');
+        })
+
+        this.el.btnClosePanelAddContact.on('click', e=> {
+
+            this.el.panelAddContact.removeClass('open');
+        })
+    }
+
+    closeAllLeftPanel() {
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
     }
 
 }
