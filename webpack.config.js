19479ce8f221f080c0f8946035abc011132b7206
João Pedro Rosa <joao.rosa5@outlook.com>
2026-05-23 21:31:55 -0300

    W15 - Refatoração da configuração do webpack para a versão 5 com webpack-cli e webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+    },
+    devServer: {
+        static: {
+            directory: __dirname
+        }
+    }
+};
