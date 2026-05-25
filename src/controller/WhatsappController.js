eeb50e74152ded04a8af06eba743007772bbab0e
João Pedro Rosa <joao.rosa5@outlook.com>
2026-05-25 00:16:42 -0300

    W32 - Implementação de seleção de contato com exibição de nome, status, foto e abertura do painel de chat

--- src/controller/WhatsappController.js
+++ src/controller/WhatsappController.js
@@ -111,6 +111,17 @@ export class WhatsAppController {
                     img.src = contact.photo;
                     img.show();
                 }
+                div.on('click', e => {
+                    this.el.activeName.innerHTML = contact.name;
+                    this.el.activeStatus.innerHTML = contact.status;
+                    if(contact.photo){
+                        let img = this.el.activePhoto;
+                        img.src = contact.photo;
+                        img.show();
+                    }
+                    this.el.home.hide();
+                    this.el.main.show();
+                });
                 this.el.contactsMessagesList.appendChild(div);
             });
         });
