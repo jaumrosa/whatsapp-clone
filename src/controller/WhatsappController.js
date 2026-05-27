0a518a34dd3a7c256a45c51457c09d72be0c0325
jaumrosa <joao.rosa5@outlook.com>
2026-05-27 02:20:13 -0300

    W47 - Exibição manual de última mensagem e horário formatado na lista de contatos via LastMessage e LastMessageTime

--- src/controller/WhatsappController.js
+++ src/controller/WhatsappController.js
@@ -84,7 +84,7 @@ export class WhatsAppController {
                                 <span dir="auto" title="${contact.name}" class="_1wjpf">${contact.name}</span>
                             </div>
                             <div class="_3Bxar">
-                                <span class="_3T2VG">${contact.LastMessageTime}</span>
+                                <span class="_3T2VG">${Format.timeStamptoTime(contact.LastMessageTime)}</span>
                             </div>
                         </div>
                         <div class="_1AwDx">
