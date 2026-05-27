53212b5d6190eada2f4b9f2d94e16ae15597f8ca
jaumrosa <joao.rosa5@outlook.com>
2026-05-26 23:51:40 -0300

    W41 - Implementação de envio de foto da câmera com espelhamento via canvas e conversão para File antes do upload

--- src/controller/WhatsappController.js
+++ src/controller/WhatsappController.js
@@ -351,6 +351,11 @@ export class WhatsAppController {
             this.el.panelCamera.css({
                 'height':'calc(100% - 120px)'
             });
+            this.el.videoCamera.show();
+            this.el.pictureCamera.hide();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
             this._camera = new CameraController(this.el.videoCamera);
         });
 
@@ -379,7 +384,38 @@ export class WhatsAppController {
         });
 
         this.el.btnSendPicture.on('click', e => {
-            console.log(this.el.pictureCamera.src);
+            this.el.btnSendPicture.disabled = true
+            const regex = /^data:(.+);base64,(.*)$/;
+            const result = this.el.pictureCamera.src.match(regex);
+            const mimeType = result[1];
+            const ext = mimeType.split('/')[1];
+            const filename = `camera${Date.now()}.${ext}`;
+            const picture = new Image();
+            picture.src = this.el.pictureCamera.src;
+            picture.onload = e => {
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d')
+                canvas.width = picture.width;
+                canvas.height = picture.height;
+                context.translate(picture.width, 0);
+                context.scale(-1, 1)
+                context.drawImage(picture, 0, 0 ,canvas.width, canvas.height);
+                fetch(canvas.toDataURL(mimeType))
+                .then(res => {return res.arrayBuffer();})
+                .then(buffer => {return new File([buffer], filename, {type: mimeType});})
+                .then(file => {
+                    Message.sendImage(this._contactActive.chatId, this._user.email, file)
+                    this.el.btnSendPicture.disabled = false;
+                    this.closeAllMainPanel();
+                    this._camera.stop();
+                    this.el.btnReshootPanelCamera.hide();
+                    this.el.pictureCamera.hide();
+                    this.el.videoCamera.hide();
+                    this.el.containerSendPicture.hide();
+                    this.el.containerTakePicture.show();
+                    this.el.panelMessagesContainer.show();
+                });
+            } 
         });
 
         this.el.btnAttachDocument.on('click', e => {
